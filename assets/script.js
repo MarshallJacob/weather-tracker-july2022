751f291d54eac574c30add90da6de3e81ec40c1c
Mjacke <marshall.jacob16@icloud.com>
2022-07-25 18:22:19 -0600

    JS improvements. Button functionality added.

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,33 @@
+// Variables pulling in the HTML elements
+const searchFormEl = document.getElementById("searchForm")
+const cityBtn = document.querySelector("cityBtn")
 const citySearchEl = document.getElementById("citySearch")
-const SearchBtnEl = document.getElementById("searchBtn")
+const cityHistoryEl = document.getElementById("cityHistory")
+const weatherInfoEl = document.getElementById("weatherInfo")
 
-SearchBtnEl.addEventListener("click", pullCityWeather(citySearchEl.id))
+function addCityList () {
+    const addCityBtn = document.createElement("button")
+    addCityBtn.classList.add("btn", "cityBtn", "btn-secondary", "mt-3");
+    addCityBtn.innerHTML = cityHistoryEl.value;
+    // const addCityBtnText = 
+    cityHistoryEl.appendChild(addCityBtn);
+};
 
-function pullCityWeather (temps) {
+searchFormEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addCityList();
+    console.log(citySearchEl.value);
+});
 
-    return console.log("This button works")
-}
+// const weatherOptions = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'b9f7a922ebmsh056b98b927e5416p1028e5jsnf1c2d7606943',
+// 		'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://community-open-weather-map.p.rapidapi.com/forecast?q=san%20francisco%2Cus', weatherOptions)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
